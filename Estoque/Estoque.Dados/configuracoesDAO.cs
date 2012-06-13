25597787f42799b80672538bda561d5a7660cabe
fabioivan.fb <fabioivan.fb@gmail.com>
2012-06-13 12:27:35 +0000

    Adição de novas classes; Implementação da classe Configuracao.cs

---
+++ Estoque/Estoque.Dados/configuracoesDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using Estoque.Module;
+
+namespace Estoque.Dados
+{
+    public class configuracoesDAO
+    {
+
+        #region Construtor
+
+        public configuracoesDAO()
+        {
+
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public bool salvaServidor(Configuracoes config)
+        {
+
+            #region Declaração
+
+            #endregion
+
+            #region Implementação
+
+            Configuration conf = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            return true;
+            #endregion
+        }
+
+        #endregion
+
+    }
+}
---
+++ Estoque/Estoque.Module/Configuracoes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Estoque.Module
+{
+    public class Configuracoes
+    {
+
+        #region Propriedades
+
+        public string servidor { get; set; }
+
+        public string database { get; set; }
+
+        public string user { get; set; }
+
+        public string senha { get; set; }
+
+        public bool conectado { get; set; }
+
+        public string conectionString { get; set; }
+
+        
+       
+        #endregion
+
+        #region Construtor
+
+        public Configuracoes()
+        {
+
+        }
+        #endregion
+
+        #region Métodos
+
+
+        #endregion
+    }
+}
