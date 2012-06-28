10a07c10d5513f3b707b4baa01e37dac32832d08
fabioivan.fb@gmail.com <fabioivan.fb@gmail.com@99469a93-d364-8502-56f7-e7a60bf2f08b>
2012-06-28 21:12:59 +0000

    Alterado o formulario de cadastro de produtos;

--- trunk/Estoque/Estoque.Module/Produto.cs
+++ trunk/Estoque/Estoque.Module/Produto.cs
@@ -5,65 +5,115 @@ using System.Text;
 
 namespace Estoque.Module
 {
-    public class Produto
+  public class Produto
+  {
+
+    #region Atributos
+
+    private int _id;
+    private string _nome;
+    private string _quantidade;
+    private string _valor;
+    private string _categoria;
+    private int _idCategoria;
+    private string _valorAPrazo;
+    private string _valorBase;
+    private string _valorCrediario;
+    private bool _disponivel;
+    private string _codigoBarras;
+    private int _idFornecedor;
+    private string _nomeFornecedor;
+    #endregion
+
+    #region Construtor
+
+    public Produto()
     {
-
-        #region Atributos
-
-        private int _id;
-        private string _nome;
-        private string _quantidade;
-        private string _valor;
-        private string _categoria;
-        private int _idCategoria;
-        #endregion
-
-        #region Construtor
-
-        public Produto()
-        {
-        }
-
-        #endregion
-
-        #region Properties
-
-        public string Categoria
-        {
-            get { return _categoria; }
-            set { _categoria = value; }
-        }
-
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        public string Nome
-        {
-            get { return _nome; }
-            set { _nome = value; }
-        }
-
-        public string Quantidade
-        {
-            get { return _quantidade; }
-            set { _quantidade = value; }
-        }
-
-        public string Valor
-        {
-            get { return _valor; }
-            set { _valor = value; }
-        }
-
-        public int IdCategoria
-        {
-          get { return _idCategoria; }
-          set { _idCategoria = value; }
-        }
-        #endregion
-
     }
+
+    #endregion
+
+    #region Properties
+
+    public string Categoria
+    {
+      get { return _categoria; }
+      set { _categoria = value; }
+    }
+
+    public int Id
+    {
+      get { return _id; }
+      set { _id = value; }
+    }
+
+    public string Nome
+    {
+      get { return _nome; }
+      set { _nome = value; }
+    }
+
+    public string Quantidade
+    {
+      get { return _quantidade; }
+      set { _quantidade = value; }
+    }
+
+    public string ValoraVista
+    {
+      get { return _valor; }
+      set { _valor = value; }
+    }
+
+    public int IdCategoria
+    {
+      get { return _idCategoria; }
+      set { _idCategoria = value; }
+    }
+
+    public string ValorAPrazo
+    {
+      get { return _valorAPrazo; }
+      set { _valorAPrazo = value; }
+    }
+
+    public string ValorBase
+    {
+      get { return _valorBase; }
+      set { _valorBase = value; }
+    }
+
+    public string ValorCrediario
+    {
+      get { return _valorCrediario; }
+      set { _valorCrediario = value; }
+    }
+
+    public bool Disponivel
+    {
+      get { return _disponivel; }
+      set { _disponivel = value; }
+    }
+
+    public string CodigoBarras
+    {
+      get { return _codigoBarras; }
+      set { _codigoBarras = value; }
+    }
+
+    public int IdFornecedor
+    {
+      get { return _idFornecedor; }
+      set { _idFornecedor = value; }
+    }
+
+    public string NomeFornecedor
+    {
+      get { return _nomeFornecedor; }
+      set { _nomeFornecedor = value; }
+    }
+
+    #endregion
+
+  }
 }
--- trunk/Estoque/Estoque.View/frmCadProdutos.cs
+++ trunk/Estoque/Estoque.View/frmCadProdutos.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using Estoque.Controller;
 using Estoque.Module;
@@ -61,7 +56,11 @@ namespace Estoque.View
       #region Declaração
       Produto produto = null;
       produtosControler produtoControl = null;
-      string valor = string.Empty;
+      string valorVista = string.Empty;
+      string valorBase = string.Empty;
+      string valorPrazo = string.Empty;
+      string valorCredito = string.Empty;
+
       #endregion
 
       #region Implementação
@@ -70,14 +69,76 @@ namespace Estoque.View
 
       produtoControl = new produtosControler();
 
-      valor = mtbValor.Text.Replace(".", "").Replace("R$", "").Trim();
+      valorVista = mtbValor.Text.Replace(".", "").Replace("R$", "").Trim();
+      valorBase = mtbValorBase.Text.Replace(".", "").Replace("R$", "").Trim();
+      valorPrazo = mtbValorPrazo.Text.Replace(".", "").Replace("R$", "").Trim();
+      valorCredito = mtbCrediario.Text.Replace(".", "").Replace("R$", "").Trim();
+
       produto.Nome = tbNomePro.Text;
       produto.IdCategoria = cbCategorias.SelectedIndex;
+      produto.CodigoBarras = tbCodBarras.Text;
       produto.Quantidade = tbQuantidade.Text;
-      produto.Valor = valor;
+      produto.Disponivel = cbxDisponivel.Checked;
+      produto.IdFornecedor = int.Parse(tbCodForne.Text);
+      produto.ValoraVista = valorVista;
+      produto.ValorBase = valorBase;
+      produto.ValorAPrazo = valorPrazo;
+      produto.ValorCrediario = valorCredito;
+
       #endregion
     }
 
+    
+
+    private void btnNovo_Click(object sender, EventArgs e)
+    {
+
+      #region Declaração
+
+      int Id = 0;
+      #endregion
+
+      #region Implementação
+
+      produtosControler prodControl = new produtosControler();
+      Id = prodControl.LastID(config);
+      Id++;
+      tbCodigoPro.Text = Convert.ToString(Id);
+
+      btnSalvar.Enabled = true;
+      btnPesForne.Enabled = false;
+      tbNomePro.Enabled = true;
+      tbCodBarras.Enabled = true;
+      tbQuantidade.Enabled = true;
+      cbCategorias.Enabled = true;
+      cbxDisponivel.Enabled = true;
+      gbFornecedor.Enabled = true;
+      gbPrecos.Enabled = true;
+
+      limpaForm();
+
+      #endregion
+    }
+
+
+    private void limpaForm()
+    {
+
+      #region Implementação
+      tbNomePro.Text = string.Empty;
+      tbCodForne.Text = string.Empty;
+      cbCategorias.Text = string.Empty;
+      tbCodBarras.Text = string.Empty;
+      tbQuantidade.Text = string.Empty;
+      tbNomeFor.Text = string.Empty;
+      mtbValor.Text = string.Empty;
+      mtbValorBase.Text = string.Empty;
+      mtbValorPrazo.Text = string.Empty;
+      mtbCrediario.Text = string.Empty;
+
+      #endregion
+
+    }
     #endregion
   }
 
